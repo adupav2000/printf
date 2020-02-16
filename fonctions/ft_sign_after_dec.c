aff2d64f686674f7b91646ba12584afad7033fec
Adupav <adupav2000@gmail.com>
2020-02-16 10:16:28 +0100

    update to 42

--- fonctions/ft_sign_after_dec.c
+++ fonctions/ft_sign_after_dec.c
@@ -6,118 +6,42 @@
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 12:16:17 by adu-pavi          #+#    #+#             */
-/*   Updated: 2020/02/01 13:53:04 by adu-pavi         ###   ########.fr       */
+/*   Updated: 2020/02/16 10:15:35 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libftprintf.h"
 
-char	*ft_sign_after_dec_just_min(int min, int len, int complement)
-{
-	char	*ret_val;
-	int		i_ret_val;
-	int		i;
-
-	i = 0;
-	if (min <= 0 || (min < len && complement == 0))
-		return (ft_strnew(0));
-	if (complement == 0)
-		i_ret_val = min - len + 1;
-	else if (complement == 1)
-		i_ret_val = min;
-	ret_val = ft_strnew(min - len + 1);
-	while (i <= i_ret_val)
-		ret_val[i++] = ' ';
-	return (ret_val);
-}
-
-char	*ft_sign_after_dec_just_max(int max, int len, char *str, void *content)
-{
-	char	*ret_val;
-
-	if (max < len)
-		return (ft_strnew(0));
-	else
-	{
-		ret_val = ft_strnew(max - len);
-		printf("%s\n", ft_get_lim_max_str(str));
-		if (str[1] == '.' || (str[1] == '0' && !ft_printfflag_has_min(str)) ||
-			!ft_strncmp("0", ft_get_lim_max_str(str), 1)
-			|| ft_printfflag_has_min(str))
-			ft_memset(ret_val, '0', (max - len));
-		else
-			ft_memset(ret_val, ' ', (max - len));
-		return (ret_val);
-	}
-	return (0);
-}
-
 char	*handle_options_positive_after(int min, int max, char *str, void *content)
 {
-	char	*tmp;
-	int		len;
 
-	len = ft_get_int_len((int)content);
-	if (min < len && max < len)
-		return (ft_strnew(0));
-	else if (min <= max)
-	{
-		if (str[1] == '.' || ft_strncmp("0", ft_get_lim_max_str(str), 1))
-			tmp = ft_strnew(max + 2);
-		ft_strlcat(tmp, ft_sign_after_dec_just_max(max, len, str, content),
-			max + 3);
-		return (tmp);
-	}
-	else if (min > max)
-	{
-		tmp = ft_strnew(min + 1);
-		ft_strlcat(tmp, ft_sign_after_dec_just_min((min - max), len, 1),
-			(min - max + 1));
-		ft_strlcat(tmp, ft_sign_after_dec_just_max(max, len, str, content),
-			(min + 1));
-		return (tmp);
-	}
 	return (0);
 }
 
-char	*handle_options_negativ_after(int min, int max, char *str, void *content)
+int ret_number_of_spaces_after_dec(int min, int max, char *str, void *content)
 {
-	char	*tmp;
 	int		len;
+	int 	ret_val;
 
-	len = ft_get_int_len((int)content);
-	if (min < len && max < len)
+	len = get_int_len((int)content);
+	ret_val = (int)content < 0 ? -1 : 0;
+	if (ft_abs(min) < len && ft_abs(max) < len)
+		return (0);
+	if (max < 0)
+		return (ret_val + ft_abs(max) - len);
+	len = (len + ret_val) < max ? max : len;
+	else if (min < 0 && ft_abs(min) > len)
 	{
-		tmp = ft_strnew(1);
-		tmp[0] = '-';
-		return (tmp);
+		if (len < max)
+			if ((max + ft_abs(ret_val)) < min)
+				return (min - (max + ft_abs(ret_val)));
+		if (len > max)
+			if ((max + ft_abs(ret_val)) < min)
+				return (min - (max + ft_abs(ret_val)));
+			return ();
 	}
-	else if (min <= max)
-	{
-		(void)ft_get_lim_max_str(str);
-		if (str[1] == '.' || ft_strncmp("0", ft_get_lim_max_str(str), 1))
-			tmp = ft_strnew(max + 2);
-		tmp[0] = '-';
-		ft_strlcat(&tmp[1], ft_sign_after_dec_just_max((max + 1), len, str,
-			content), max + 3);
-		return (tmp);
-	}
-	else
-	{
-		tmp = ft_strnew(min - len + 1);
-		ft_strlcat(tmp, ft_sign_after_dec_just_min((min - max), len, 1),
-			(min - max + 1));
-		if (str[1] == '.' || ft_strncmp("0", ft_get_lim_max_str(str), 2) ||
-			ft_printfflag_has_min(str))
-			tmp[ft_strlen(tmp)] = '-';
-		ft_strlcat(tmp, ft_sign_after_dec_just_max(max, len, str, content),
-			(max + 1));
-		if (!(str[1] == '.' || ft_strncmp("0", ft_get_lim_max_str(str), 2) ||
-			ft_printfflag_has_min(str)))
-			tmp[ft_strlen(tmp)] = '-';
-		return (tmp);
-	}
-	return (0);
+
+	return (ret_val);
 }
 
 char	*ft_sign_after_dec(char *str, void *content)
@@ -129,11 +53,11 @@ char	*ft_sign_after_dec(char *str, void *content)
 	len = ft_get_int_len((int)content);
 	if (ft_printfflag_has_min(str))
 		min = ft_get_lim_string_min(str);
-	if (!ft_printfflag_has_min(str) || min < 0)
+	if (!ft_printfflag_has_min(str))
 		min = 0;
 	if (ft_printfflag_has_max(str))
 		max = ft_get_lim_string_max(str);
-	if (!ft_printfflag_has_max(str) || max < 0)
+	if (!ft_printfflag_has_max(str))
 		max = 0;
 	if ((int)content >= 0)
 		return (handle_options_positive_after(min, max, len, str));
@@ -163,5 +87,8 @@ positif : afficher un espace puis le chiffre puis le reste de 0 ++++
 Il y a un espace puis un 0,
 négatif : afficher un -, le nombre de 0 manquant et le nombre +++++
 positif : afficher un espace puis le reste de 0 et le nombre ++++
+
+
+si il y a un max négatif le min est ignoré
 
 */
--- fonctions/test.c
+++ fonctions/test.c
@@ -6,7 +6,7 @@
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 15:23:45 by adu-pavi          #+#    #+#             */
-/*   Updated: 2020/02/15 18:40:45 by adu-pavi         ###   ########.fr       */
+/*   Updated: 2020/02/16 10:15:11 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,108 +15,139 @@
   #include <unistd.h>
   #include <stdio.h>
 
-  #include "../libftprintf.h"
+  // #include "../libftprintf.h"
   
-  #include "define_conv_ptr.c"
-  #include "ft_isalpha.c"
-  #include "ft_memcmp.c"
-  #include "ft_putstr.c"
-  #include "ft_strdel.c"
-  #include "ft_strmap.c"
-  #include "ft_strtrim.c"
-  #include "conv_char.c"
-  #include "ft_isascii.c"
-  #include "ft_memcpy.c"
-  #include "ft_putstr_fd.c"
-  #include "ft_strdup.c"
-  #include "ft_strmapi.c"
-  #include "ft_substr.c"
-  #include "conv_dec.c"
-  #include "ft_isdigit.c"
-  #include "ft_memdel.c"
-  #include "ft_rev_str.c"
-  #include "ft_strequ.c"
-  #include "ft_strncat.c"
-  #include "ft_tolower.c"
-  #include "conv_from_base.c"
-  #include "ft_atoi.c"
-  #include "ft_islower.c"
-  #include "ft_memmove.c"
-  #include "ft_split.c"
-  #include "ft_striter.c"
-  #include "ft_strncmp.c"
-  #include "ft_toupper.c"
-  #include "conv_hexa_from_void.c"
-  #include "ft_bzero.c"
-  #include "ft_isprint.c"
-  #include "ft_memset.c"
-  #include "ft_str_count_opt.c"
-  #include "ft_striteri.c"
-  #include "ft_strncpy.c"
-  #include "get_int_len.c"
-  #include "conv_maj_hex.c"
-  #include "ft_calloc.c"
-  #include "ft_isspace.c"
-  #include "ft_occure_in_set.c"
-  #include "ft_strcat.c"
-  #include "ft_strjoin.c"
-  #include "ft_strnequ.c"
+  // #include "define_conv_ptr.c"
+  // #include "ft_isalpha.c"
+  // #include "ft_memcmp.c"
+  // #include "ft_putstr.c"
+  // #include "ft_strdel.c"
+  // #include "ft_strmap.c"
+  // #include "ft_strtrim.c"
+  // #include "conv_char.c"
+  // #include "ft_isascii.c"
+  // #include "ft_memcpy.c"
+  // #include "ft_putstr_fd.c"
+  // #include "ft_strdup.c"
+  // #include "ft_strmapi.c"
+  // #include "ft_substr.c"
+  // #include "conv_dec.c"
+  // #include "ft_isdigit.c"
+  // #include "ft_memdel.c"
+  // #include "ft_rev_str.c"
+  // #include "ft_strequ.c"
+  // #include "ft_strncat.c"
+  // #include "ft_tolower.c"
+  // #include "conv_from_base.c"
+  // #include "ft_atoi.c"
+  // #include "ft_islower.c"
+  // #include "ft_memmove.c"
+  // #include "ft_split.c"
+  // #include "ft_striter.c"
+  // #include "ft_strncmp.c"
+  // #include "ft_toupper.c"
+  // #include "conv_hexa_from_void.c"
+  // #include "ft_bzero.c"
+  // #include "ft_isprint.c"
+  // #include "ft_memset.c"
+  // #include "ft_str_count_opt.c"
+  // #include "ft_striteri.c"
+  // #include "ft_strncpy.c"
+  // #include "get_int_len.c"
+  // #include "conv_maj_hex.c"
+  // #include "ft_calloc.c"
+  // #include "ft_isspace.c"
+  // #include "ft_occure_in_set.c"
+  // #include "ft_strcat.c"
+  // #include "ft_strjoin.c"
+  // #include "ft_strnequ.c"
 
-  #include "conv_min_hex.c"
-  #include "ft_exp.c"
-  #include "ft_isupper.c"
-  #include "ft_putchar.c"
-  #include "ft_strlcat.c"
-  #include "ft_strchr.c"
-  #include "ft_strnew.c"
+  // #include "conv_min_hex.c"
+  // #include "ft_exp.c"
+  // #include "ft_isupper.c"
+  // #include "ft_putchar.c"
+  // #include "ft_strlcat.c"
+  // #include "ft_strchr.c"
+  // #include "ft_strnew.c"
 
-  #include "conv_nothing.c"
-  #include "ft_get_int_char_length.c"
-  #include "ft_itoa.c"
-  #include "ft_putchar_fd.c"
-  #include "ft_strclr.c"
-  #include "ft_strlcpy.c"
-  #include "ft_strnlen.c"
-  #include "ret_type.c"
+  // #include "conv_nothing.c"
+  // #include "ft_get_int_char_length.c"
+  // #include "ft_itoa.c"
+  // #include "ft_putchar_fd.c"
+  // #include "ft_strclr.c"
+  // #include "ft_strlcpy.c"
+  // #include "ft_strnlen.c"
+  // #include "ret_type.c"
 
-  #include "conv_string.c"
-  #include "ft_get_lim.c"
-  #include "ft_memalloc.c"
-  #include "ft_putendl.c"
-  #include "ft_strcmp.c"
-  #include "ft_strlen.c"
-  #include "ft_strnstr.c"
+  // #include "conv_string.c"
+  // #include "ft_get_lim.c"
+  // #include "ft_memalloc.c"
+  // #include "ft_putendl.c"
+  // #include "ft_strcmp.c"
+  // #include "ft_strlen.c"
+  // #include "ft_strnstr.c"
  
-  #include "conv_unsigned_dec.c"
-  #include "ft_is_used_letter.c"
-  #include "ft_memccpy.c"
-  #include "ft_putendl_fd.c"
-  #include "ft_strlenopt.c"
-  #include "ft_strrchr.c"
-  #include "ft_increment_til_text.c"
-  #include "printf.c"
-  #include "ft_sign_before_dec.c"
-  #include "ft_get_len_conv_dec.c"
-  #include "ft_get_lim_string_min.c"
-  #include "ft_get_lim_string_max.c"
-  #include "ft_shift_char_in_string.c"
-  #include "ft_isprintf_flag.c"
-  #include "ft_printfflag_has_min.c"
-  #include "ft_printfflag_has_max.c"
-  #include "ft_get_signs_before_dec.c"
-  #include "ft_abs.c"
-  #include "ft_sign_after_dec.c"
-  #include "ft_handle_stars.c"
-  #include "ft_jump_to_next_arg.c"
-  #include "ft_get_lim_max_str.c"
-  #include "ft_get_lim_min_str.c"
+  // #include "conv_unsigned_dec.c"
+  // #include "ft_is_used_letter.c"
+  // #include "ft_memccpy.c"
+  // #include "ft_putendl_fd.c"
+  // #include "ft_strlenopt.c"
+  // #include "ft_strrchr.c"
+  // #include "ft_increment_til_text.c"
+  // #include "printf.c"
+  // #include "ft_sign_before_dec.c"
+  // #include "ft_get_len_conv_dec.c"
+  // #include "ft_get_lim_string_min.c"
+  // #include "ft_get_lim_string_max.c"
+  // #include "ft_shift_char_in_string.c"
+  // #include "ft_isprintf_flag.c"
+  // #include "ft_printfflag_has_min.c"
+  // #include "ft_printfflag_has_max.c"
+  // #include "ft_get_signs_before_dec.c"
+  // #include "ft_abs.c"
+  // #include "ft_sign_after_dec.c"
+  // #include "ft_handle_stars.c"
+  // #include "ft_jump_to_next_arg.c"
+  // #include "ft_get_lim_max_str.c"
+  // #include "ft_get_lim_min_str.c"
 
 int main()
 {
   int a;
   a = 0;
-  printf("___printf = %2.-11d\n", 412);
-  ft_printf("ft_printf = %2.-11d\n", 412);
+  printf("___printf = -011d = %-011d\n", 412);
+  printf("___printf = -011d = %-011d\n", -412);
+  printf("___printf = -5.2d = %-5.2d\n", 412);
+  printf("___printf = -5.2d = %-5.2d\n", -412);
+  printf("___printf = -01d = %-01d\n", 412);
+  printf("___printf = -1d = %-01d\n", -412);
+  printf("___printf = 22.-11d = %22.-11d\n", 412);
+  printf("___printf = 22.-11d = %22.-11d\n", -412);
+  printf("___printf = -2.11d = %-2.11d\n", 412);
+  printf("___printf = -2.11d = %-2.11d\n", -412);
+  printf("___printf = -22.11d = %-22.11d\n", 412);
+  printf("___printf = -22.11d = %-22.11d\n", -412);
+  printf("___printf = 2.-11d = %2.-11d\n", 412);
+  printf("___printf = 2.-11d = %2.-11d\n", -412);
+  printf("___printf = 2.-1d = %2.-1d\n", 412);
+  printf("___printf = 2.-1d = %2.-1d\n", -412);
+  printf("___printf = -2.-11d = %-2.-11d\n", 412);
+  printf("___printf = -2.-11d = %-2.-11d\n", -412);
+  printf("___printf = -22.-1d = %-22.-1d\n", 412);
+  printf("___printf = -22.-1d = %-22.-1d\n", -412);
+  printf("___printf = -22.-11d = %-22.-11d\n", 412);
+  printf("___printf = -22.-11d = %-22.-11d\n", -412);
+
+
+
+
+
+
+
+
+
+  
+  
   // printf("%.15d\n", -412);
   // printf("%15d\n", 412);
   return (0);
